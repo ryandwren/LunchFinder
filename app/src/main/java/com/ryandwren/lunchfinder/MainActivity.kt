e64526bee69e310a44d269ddb72d628a19f87050
ryandwren <ryandwren@gmail.com>
2024-10-25 20:59:54 -0400

    BottomBar with navigation

--- app/src/main/java/com/ryandwren/lunchfinder/MainActivity.kt
+++ app/src/main/java/com/ryandwren/lunchfinder/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ryandwren.lunchfinder.ui.ListScreen
+import com.ryandwren.lunchfinder.ui.MapScreen
 import com.ryandwren.lunchfinder.ui.theme.LunchFinderTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,69 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LunchFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                val navController = rememberNavController()
+
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding(),
+                    topBar = {},
+                    bottomBar = {
+                        HorizontalDivider()
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+
+                            Row(
+                                modifier = Modifier
+                                    .clickable {
+                                        navController.navigate(ScreenNames.LIST)
+                                    }
+                                    .padding(vertical = 16.dp, horizontal = 48.dp)
+                            ) {
+                                Text(
+                                    text = "List"
+                                )
+                            }
+
+                            Row(
+                                modifier = Modifier
+                                    .clickable {
+                                        navController.navigate(ScreenNames.MAP)
+                                    }
+                                    .padding(vertical = 16.dp, horizontal = 48.dp)
+                            ) {
+                                Text(
+                                    text = "Map"
+                                )
+                            }
+                        }
+                    },
+                )
+                { innerPadding ->
+                    NavHost(
+                        navController,
+                        startDestination = ScreenNames.LIST,
+                        Modifier.padding(innerPadding)
+                    ) {
+                        composable(ScreenNames.LIST) {
+                            ListScreen()
+                        }
+
+                        composable(ScreenNames.MAP) {
+                            MapScreen()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LunchFinderTheme {
-        Greeting("Android")
-    }
+object ScreenNames {
+    const val LIST = "List"
+    const val MAP = "Map"
 }
